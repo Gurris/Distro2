cf3f67389001cb6281b7198c32fb786eb06243d8
Gustaf Holmström <gustaf.holmstrom@hotmail.com>
2016-10-07 21:22:19 +0200

    MVC - template

---
+++ Distro2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Distro2.Startup))]
+namespace Distro2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
